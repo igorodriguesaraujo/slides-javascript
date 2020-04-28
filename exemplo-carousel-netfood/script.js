c6d273497bffba4867c488a084ec4fcc097cb82e
Igor Rodrigues <igorodrigues_@hotmail.com>
2020-04-28 00:37:19 -0300

    Modificado: refatorando code v2

--- exemplo-carousel-netfood/script.js
+++ exemplo-carousel-netfood/script.js
@@ -20,48 +20,53 @@ function nextSlide() {
 
 // Previous Slide
 function prevSlide() {
-  if (count <= 0) {
-    console.log('Estamos no inicio')
-  } else {
+  if (!count <= 0) {
     count--;
     let position = slides.clientWidth * count;
     parent.style.transform = `translateX(-${position}px)`;
   }
 }
+
 // Scale Image
 function scaleImage() {
   let current = slides.children;
 
   for (let i = 0; i <= current.length; i++) {
 
-    let next = i + 1;
-    let previous = i - 1;
+    let next = i + 1,
+      previous = i - 1,
+      scaleIn = "scale(1.3)",
+      scaleOut = "scale(1)";
 
     current[i].addEventListener("mouseover", (event) => {
       if (i === 0) {
-        event.currentTarget.style.transform = "scale(1.3)";
+        event.currentTarget.style.transform = scaleIn;
         current[next].style.marginLeft = "38px";
+
       } else if (i >= current.length - 1) {
-        event.currentTarget.style.transform = "scale(1.3)";
+        event.currentTarget.style.transform = scaleIn;
         current[previous].style.marginRight = "43px";
+
       } else {
         current[previous].style.marginRight = "43px";
-        event.currentTarget.style.transform = "scale(1.3)";
+        event.currentTarget.style.transform = scaleIn;
         current[next].style.marginLeft = "38px";
       }
     })
 
     current[i].addEventListener("mouseleave", (event) => {
       if (i === 0) {
-        event.currentTarget.style.transform = "scale(1)";
-        current[next].style.marginLeft = "0px";
+        event.currentTarget.style.transform = scaleOut;
+        current[next].style.marginLeft = "";
+
       } else if (i >= current.length - 1) {
-        event.currentTarget.style.transform = "scale(1)";
+        event.currentTarget.style.transform = scaleOut;
         current[previous].style.marginRight = "5px";
+
       } else {
         current[previous].style.marginRight = "5px";
-        event.currentTarget.style.transform = "scale(1)";
-        current[next].style.marginLeft = "0px";
+        event.currentTarget.style.transform = scaleOut;
+        current[next].style.marginLeft = "";
       }
 
     })
